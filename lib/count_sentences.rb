fc6c49cbbab62cf5b216928cacb26a160e79bb7a
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-26 15:36:34 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array=self.split(/[.?!]/)
+    array.each_with_index do |n, i|
+      if n == ""
+        array.delete_at(i)
+      end
+    end
+    array.count
   end
 end
